9136c805d2aae9e953bf9ff96d075343c44c08a7
dempseyucla <dempseyucla@gmail.com>
2026-01-20 16:40:26 -0800

    Version 2. Commit 2 for a11

--- a11.c
+++ a11.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 int one();
+int two();
 
 int main()
 {
     printf("main\n");
     one();
+    two();
     printf("This line was done on GITHUB 15\n");
     printf("IT'S PEOPLE!!");
 }
